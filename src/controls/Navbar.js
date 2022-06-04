96610efa1b0b8f759385a828166c6d8bccf52838
IAndronov <Igor.Andronov@alfabank.kiev.ua>
2022-06-04 10:24:54 +0300

    about

--- src/controls/Navbar.js
+++ src/controls/Navbar.js
@@ -1,5 +1,8 @@
 import Gallery from "./Gallery";
 import './Navbar.css'
+import pic1 from '../pic/lina.jpg'
+import pic2 from '../pic/pic2.jpg'
+import pic3 from '../pic/pic3.jpg'
 
 import {useState} from "react";
 
@@ -47,15 +50,37 @@ function Navbar(props) {
 
             <div className="about" id="about" style={{display: aboutVisibility}}>
                 <h3>ART BY LINA DONETS</h3>
-                <p>ABOUT</p>
+                <img className="img"
+                     src={pic1}
+                     style={{maxWidth:"90%"}}
+                />
+
                 <p>Story Behind the Art</p>
                 <span>
-                    I am largely a self-taught artist with my best teacher being the great outdoors. My sincere wish to become a painter was always blocked by real life commitments but the desire never left me. With the required time now available I am pursuing this ambition, ultimately painting in oils this universe. I’m originally from Ukraine living in Germany. Hope, soon I’ll be back home. Glory to Ukraine!
-                I am an exuberant artist and paint in my own fashion as broadly as my mood allows with spirit and fantasy sense. My favorite color is white and I love when there are a lot of cream oils on my canvas what allows me to create an unusual texture, relief and line by sculpting oil paint with my palette knife.
-                My work always tries to demonstrate the inspiring connection and response to my real  surroundings. I strive to create paintings which celebrate life and excite the soul. My aim is for people to feel good and peaceful when looking at my creations.
-                I hope that my work transports you to another place and time.
-                Enjoy what is created when my palette knife applies paint to canvas!
+                    <p>
+                        I am largely a self-taught artist with my best teacher being the great outdoors. My sincere wish to become a painter was always blocked by real life commitments but the desire never left me. With the required time now available I am pursuing this ambition, ultimately painting in oils this universe. I’m originally from Ukraine living in Germany. Hope, soon I’ll be back home. Glory to Ukraine!
+                    </p>
+                    <p>
+                        I am an exuberant artist and paint in my own fashion as broadly as my mood allows with spirit and fantasy sense. My favorite color is white and I love when there are a lot of cream oils on my canvas what allows me to create an unusual texture, relief and line by sculpting oil paint with my palette knife.
+                    </p>
+                    <p>
+                        My work always tries to demonstrate the inspiring connection and response to my real  surroundings. I strive to create paintings which celebrate life and excite the soul. My aim is for people to feel good and peaceful when looking at my creations.
+                    </p>
+                    <p>
+                        I hope that my work transports you to another place and time.
+                    </p>
+                    <p>
+                        Enjoy what is created when my palette knife applies paint to canvas!
+                    </p>
                 </span>
+                <br/>
+                <img className="img" style={{maxWidth:"30%", marginLeft:"auto", marginRight:"auto", marginTop:"20px", float:"left"}}
+                     src={pic2}
+                />
+                <img className="img" style={{maxWidth:"34.5%", marginLeft:"20px", marginRight:"auto", marginTop:"20px", float:"left"}}
+                     src={pic3}
+                />
+
             </div>
         </div>
 
